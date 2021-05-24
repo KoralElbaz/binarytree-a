856e0f59ae7be0fae70f759f093aaa5ff7b90eb0
KoralElbaz <73183167+KoralElbaz@users.noreply.github.com>
2021-05-24 21:38:27 +0300

    Add files via upload

---
+++ Test.cpp
@@ -0,0 +1,156 @@
+#include "doctest.h"
+#include "BinaryTree.hpp"
+#include <string>
+#include <iostream>
+
+using namespace std;
+using namespace ariel;
+
+//BinaryTree<int> tree_of_ints;
+
+class Color
+{
+public:
+    string color_name;
+    Color(string c) : color_name(c){};
+    bool operator==(const Color &n) const
+    {
+        return this->color_name == n.color_name;
+    };
+};
+
+BinaryTree<Color> binary_tree_color_generator()
+{
+    BinaryTree<Color> bt;
+    bt.add_root(Color("pink"))
+        .add_left(Color("pink"), Color("blue"))
+        .add_left(Color("blue"), Color("orange"))
+        .add_right(Color("blue"), Color("green"))
+        .add_right(Color("pink"), Color("red"))
+        .add_left(Color("pink"), Color("yellow"));
+    return bt;
+      /* Prints the tree in a reasonable format. For example:
+        pink
+        |-------------|
+        yellow       red
+        |-------------|
+        orange      green
+  */
+}
+
+
+//---------------------------------------  int  --------------------------------------------//
+TEST_CASE("check preorder by int")
+{
+    BinaryTree<int> tree_of_ints;
+    for (int i = 1; i < 10; i++) {
+        tree_of_ints.add_left(i, i + 1);
+    }
+    int j = 1;
+    for (auto it = tree_of_ints.begin_preorder(); it != tree_of_ints.end_preorder() || j<10; it++,j++) {
+                CHECK((*it) == j);
+       
+    }
+}
+
+TEST_CASE("check preorder by int")
+{
+    BinaryTree<int> tree_of_ints;
+    for (int i = 1; i < 10; i++) {
+        tree_of_ints.add_right(i, i + 1);
+    }
+    int j = 1;
+    for (auto it = tree_of_ints.begin_preorder(); it != tree_of_ints.end_preorder() || j<10; it++,j++) {
+                CHECK((*it) == j);
+       
+    }
+}
+
+
+//--------------------------------------- object Color --------------------------------------------//
+
+TEST_CASE("check preorder by object")
+{
+    BinaryTree<Color> tree_of_color = binary_tree_color_generator();
+
+    Color arr_colors[] = {Color("pink"), Color("yellow"), Color("orange"), Color("green"), Color("red")};
+    int j = 0;
+    for (auto it = tree_of_color.begin_preorder(); it != tree_of_color.end_preorder() && j < 5; ++it, j++)
+    {
+        CHECK((*it) == arr_colors[j]);
+    }
+}
+
+TEST_CASE("check inorder by object")
+{
+    BinaryTree<Color> tree_of_color = binary_tree_color_generator();
+
+    Color arr_colors[] = {Color("orange"), Color("yellow"), Color("green"), Color("pink"), Color("red")};
+    int j = 0;
+    for (auto it = tree_of_color.begin_preorder(); it != tree_of_color.end_preorder() && j < 5; ++it, j++)
+    {
+        CHECK((*it) == arr_colors[j]);
+    }
+}
+
+TEST_CASE("check postorder by object ")
+{
+    BinaryTree<Color> tree_of_color = binary_tree_color_generator();
+
+    Color arr_colors[] = {Color("orange"), Color("green"), Color("yellow"), Color("red"), Color("pink")};
+    int j = 0;
+
+    for (auto it = tree_of_color.begin_preorder(); it != tree_of_color.end_preorder() && j < 5; ++it, j++)
+    {
+        CHECK((*it) == arr_colors[j]);
+    }
+}
+
+
+TEST_CASE("change the int tree ")
+{
+    BinaryTree<int> tree_of_ints;
+    CHECK_NOTHROW(tree_of_ints.add_root(1));
+
+    CHECK_NOTHROW(tree_of_ints.add_left(1, 2));
+    CHECK_NOTHROW(tree_of_ints.add_right(1, 3));
+
+    CHECK_NOTHROW(tree_of_ints.add_left(2, 4));
+    CHECK_NOTHROW(tree_of_ints.add_right(2, 5));
+
+    CHECK_NOTHROW(tree_of_ints.add_left(3, 6));
+    CHECK_NOTHROW(tree_of_ints.add_right(3, 7));
+
+    CHECK_NOTHROW(tree_of_ints.add_right(5, 3));
+    CHECK_NOTHROW(tree_of_ints.add_left(1, 9));
+
+    CHECK_THROWS(tree_of_ints.add_right(10, 11)); // 10 not in the tree
+    CHECK_THROWS(tree_of_ints.add_left(12, 4)); // 12 not in the tree
+    CHECK_THROWS(tree_of_ints.add_left(2, 18)); // 2 not in the tree 
+
+}
+
+TEST_CASE("change the char tree")
+{
+    BinaryTree<char> tree_of_char;
+    CHECK_NOTHROW(tree_of_char.add_root('r'));
+    CHECK_NOTHROW(tree_of_char.add_right('r', 'h'));
+    CHECK_NOTHROW(tree_of_char.add_right('h', 'y'));
+
+    CHECK_NOTHROW(tree_of_char.add_left('r', 'f'));
+    CHECK_NOTHROW(tree_of_char.add_left('f', 'g'));
+
+    CHECK_NOTHROW(tree_of_char.add_right('f', 'b'));
+   
+    CHECK_NOTHROW(tree_of_char.add_right('y', 's'));
+ 
+    CHECK_NOTHROW(tree_of_char.add_left('s', 'a'));
+    CHECK_THROWS(tree_of_char.add_right('z', 'g'));
+
+    CHECK_THROWS(tree_of_char.add_left('q', 'a'));
+    CHECK_NOTHROW(tree_of_char.add_root('n'));
+
+    CHECK_THROWS(tree_of_char.add_right('p', 'y')); // p not in the tree
+    CHECK_THROWS(tree_of_char.add_left('l', 'a'));// l not in the tree
+
+}
---
+++ sources/BinaryTree.hpp
@@ -0,0 +1,285 @@
+#pragma one
+#include <memory>
+
+//std::shared_ptr<Node>
+namespace ariel
+{
+
+    //--------------------------------------- Node class -------------------------------------//
+    template <typename T>
+    class BinaryTree
+    {
+        class Node
+        {
+            friend class BinaryTree;
+            T data;
+            Node *left;
+            Node *right;
+            Node *parent;
+            Node(T val) : data(val), right(NULL), left(NULL), parent(NULL){};
+        };
+
+        Node *root;
+
+    public:
+        BinaryTree() : root(nullptr) {}
+        friend std::ostream &operator<<(std::ostream &os, const BinaryTree<T> &tree)
+        {
+            return os;
+        }
+        BinaryTree<T> &add_root(T data)
+        {
+            if (root != nullptr) // There is already a root just need to update a value
+            {
+                root->data = data;
+            }
+            else // We will create a new one
+            {
+                this->root = new Node(data);
+            }
+            return *this;
+        }
+
+        Node *found_node(Node *node, T value)
+        {
+            if (node == nullptr) //Basic
+            {
+                return nullptr;
+            }
+            if (node->data == value) // we found the value !!!!!
+            {
+                return node;
+            }
+            Node *right_node = nullptr;
+            Node *left_node = nullptr;
+            if (node->left != nullptr) // if we can move left
+            {
+                left_node = found_node(node->left, value);
+            }
+            if (node->right != nullptr) // if we can move right
+            {
+                right_node = found_node(node->right, value);
+            }
+            // If the tree is not symmetrical
+            if (left_node != nullptr)
+            {
+                return left_node;
+            }
+            else
+            {
+                return right_node;
+            }
+        };
+
+        // Node *find_node(T value){
+        //     if(root->value==value)
+        //     {
+        //         return root;
+        //     }
+        //     return find_node(root,val);
+        // };
+
+        BinaryTree<T> &add_left(T parent_val, T child_val)
+        {
+            Node *found = found_node(this->root, parent_val);
+            if (found == nullptr) // if we didnt found
+            {
+                throw std::invalid_argument("No value found");
+            }
+            else
+            {
+                if (found->left == nullptr) //If he has no left son
+                {
+                    found->left = new Node(child_val);
+                    found->left->parent = found; // Updating his parent
+                }
+                else // If he has a son
+                {
+                    found->left->data = child_val;
+                }
+            }
+            return *this;
+        }
+
+        BinaryTree<T> &add_right(T parent_val, T child_val)
+        {
+            Node *found = found_node(this->root, parent_val);
+            if (found == nullptr) // if we didnt found
+            {
+                throw std::invalid_argument("No value found");
+            }
+            else
+            {
+                if (found->right == nullptr) //If he has no right son
+                {
+                    found->right = new Node(child_val);
+                    found->right->parent = found; // Updating his parent
+                }
+                else // If he has a son
+                {
+                    found->right->data = child_val;
+                }
+            }
+            return *this;
+        }
+
+        //--------------------------------------- Preorder class -------------------------------------//
+
+        class Preorder_iter
+        {
+        private:
+            Node *curr;
+
+        public:
+            Preorder_iter(Node *ptr = nullptr) : curr(ptr){};
+            Preorder_iter(): curr(nullptr){}
+
+            T &operator*() const
+            {
+                return curr->data;
+            }
+
+            T *operator->() const
+            {
+                return &(curr->data);
+            }
+            Preorder_iter &operator++()
+            {
+                return *this;
+            }
+            const Preorder_iter operator++(int)
+            {
+                return *this;
+            }
+            bool operator==(const Preorder_iter &other) const
+            {
+                return curr == other.p_ccurrurr;
+            }
+
+            bool operator!=(const Preorder_iter &other) const
+            {
+                return curr != other.curr;
+            }
+        };
+        //--------------------------------------- Inorder class -------------------------------------//
+
+        class Inorder_iter
+        {
+        private:
+            Node *curr;
+
+        public:
+            Inorder_iter(Node *ptr = nullptr) : curr(ptr){};
+            Inorder_iter(): curr(nullptr){}
+
+
+            T &operator*() const
+            {
+                return curr->data;
+            }
+
+            T *operator->() const
+            {
+                return &(curr->data);
+            }
+            Inorder_iter &operator++()
+            {
+                return *this;
+            }
+            const Inorder_iter operator++(int)
+            {
+                return *this;
+            }
+            bool operator==(const Inorder_iter &other) const
+            {
+                return curr == other.p_ccurrurr;
+            }
+
+            bool operator!=(const Inorder_iter &other) const
+            {
+                return curr != other.curr;
+            }
+        };
+
+        //--------------------------------------- Postorder class -------------------------------------//
+
+        class Postorder_iter
+        {
+        private:
+            Node *curr;
+
+        public:
+            Postorder_iter(Node *ptr = nullptr) : curr(ptr){};
+            Postorder_iter():curr(nullptr){}
+
+
+            T &operator*() const
+            {
+                return curr->data;
+            }
+
+            T *operator->() const
+            {
+                return &(curr->data);
+            }
+            Postorder_iter &operator++()
+            {
+                return *this;
+            }
+            const Postorder_iter operator++(int)
+            {
+                return *this;
+            }
+            bool operator==(const Postorder_iter &other) const
+            {
+                return curr == other.p_ccurrurr;
+            }
+
+            bool operator!=(const Postorder_iter &other) const
+            {
+                return curr != other.curr;
+            }
+        };
+
+        //--------------------------------------- Tree class -------------------------------------//
+
+        // void push(T data)
+        // {
+        //     auto temp = make_shared<Node>();
+        //     temp->data = data;
+        // }
+
+        Inorder_iter begin()
+        {
+            return begin_inorder();
+        };
+        Inorder_iter end()
+        {
+            return end_inorder();
+        };
+        Preorder_iter end_preorder()
+        {
+            return Preorder_iter(nullptr);
+        }
+        Preorder_iter begin_preorder()
+        {
+            return Preorder_iter(root);
+        }
+        Inorder_iter end_inorder()
+        {
+            return Inorder_iter(nullptr);
+        }
+        Inorder_iter begin_inorder()
+        {
+            return Inorder_iter(root);
+        }
+        Postorder_iter end_postorder()
+        {
+            return Postorder_iter(root);
+        }
+        Postorder_iter begin_postorder()
+        {
+            return Postorder_iter(nullptr);
+        }
+    };
+}
